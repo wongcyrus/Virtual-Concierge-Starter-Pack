4b559c8a9dc0beeb143710d6b6aa9b95022025d5
EC2 Default User <ec2-user@ip-172-31-90-124.ec2.internal>
2018-09-13 06:26:27 +0000

    Instruction to deploy and reorder the command to give time for Lex Import.

--- README.md
+++ README.md
@@ -6,4 +6,16 @@ Deployment Script set up Lex Chatbot and run CloudFormation.
 CloudFormation creates CognitoIdentityPool, S3Bucket for static content, DynamoDB (SumerianConciergeExperienceUsers). 
 
 
+Create a Cloud 9 environment and open terminal.
 
+
+git clone https://github.com/wongcyrus/Virtual-Concierge-Starter-Pack.git
+
+
+cd Virtual-Concierge-Starter-Pack.git
+
+
+chmod +x deployment.sh
+
+
+./deployment.sh
--- deplyment.sh
+++ deplyment.sh
@@ -4,13 +4,13 @@ rm -rf __MACOSX/
 pip install awscli --upgrade --user
 aws iam create-service-linked-role --aws-service-name lex.amazonaws.com
 aws lex-models start-import --payload fileb://./aws-resources/resources/lex/SumerianConciergeConversation-Lex.zip --resource-type BOT --merge-strategy OVERWRITE_LATEST
-sleep 10
+aws cloudformation create-stack --stack-name sumerianconcierge --template-body file://SumerianConciergeExperience-CloudFormationTemplate.yml --capabilities CAPABILITY_IAM
+aws cloudformation wait stack-create-complete --stack-name sumerianconcierge
 aws lex-models put-bot-alias --name Prod --bot-name SumerianConciergeConversation --bot-version "\$LATEST"
+sleep 10
 lex_checksum=$(aws lex-models get-bot --name SumerianConciergeConversation --version-or-alias "\$LATEST" --query 'checksum' --output text)
 sed -i "s/###checksum###/$lex_checksum/g" SumerianConciergeConversation.json
 aws lex-models put-bot --name SumerianConciergeConversation --cli-input-json file://SumerianConciergeConversation.json
-aws cloudformation create-stack --stack-name sumerianconcierge --template-body file://SumerianConciergeExperience-CloudFormationTemplate.yml --capabilities CAPABILITY_IAM
-aws cloudformation wait stack-create-complete --stack-name sumerianconcierge
 S3Bucket=$(aws cloudformation describe-stacks --stack-name sumerianconcierge --query 'Stacks[0].Outputs[?OutputKey==`S3Bucket`].OutputValue' --output text)
 wget -P aws-resources/images/ https://raw.githubusercontent.com/inspirit/jsfeat/master/cascades/frontalface.js 
 wget -P aws-resources/images/ https://raw.githubusercontent.com/inspirit/jsfeat/master/build/jsfeat-min.js 
